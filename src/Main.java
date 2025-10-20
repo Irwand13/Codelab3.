5af401a7c744656ae610690f2b6e481a27b412f8
Irwanda Agus <Irwanff41@gmail.com>
2025-10-20 11:10:05 +0700

    update menu 5

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Menu menu2 = new Menu("Ayam Bakar Madu", 25000);
         Menu menu3 = new Menu("Es Teh Manis", 5000);
         Menu menu4 = new Menu("Es Jeruk", 5000) ;
+        Menu menu5 = new Menu ("ES Anget", 5000) ;
         Order pesanan = new Order();
         pesanan.tambahPesanan(menu1);
         pesanan.tambahPesanan(menu3);
